bbca22d21ad244d6c67c34f4d943a1a0b4b4882b
mrozma85 <98060069+mrozma85@users.noreply.github.com>
2024-06-27 22:39:28 +0200

    End of section 2

---
+++ -- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, 'Maciej');
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, 'Tomek');
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, 'Weronika');
