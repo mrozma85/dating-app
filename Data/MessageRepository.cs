267369c3f7be4f05bef43e171a472f9230694f82
mrozma85 <98060069+mrozma85@users.noreply.github.com>
2024-10-08 22:35:29 +0200

    End of section 17

--- Data/MessageRepository.cs
+++ Data/MessageRepository.cs
@@ -11,6 +11,11 @@ namespace API.Data;
 
 public class MessageRepository(DataContext context, IMapper mapper) : IMessageRepository
 {
+    public void AddGroup(Group group)
+    {
+        context.Groups.Add(group);
+    }
+
     public void AddMessage(Message message)
     {
         context.Messages.Add(message);
@@ -21,9 +26,29 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
         context.Messages.Remove(message);
     }
 
+    public async Task<Connection?> GetConnection(string connectionId)
+    {
+        return await context.Connections.FindAsync(connectionId);
+    }
+
+    public async Task<Group?> GetGroupForConnection(string connectionId)
+    {
+        return await context.Groups
+            .Include(x => x.Connections)
+            .Where(XmlConfigurationExtensions => XmlConfigurationExtensions.Connections.Any(c => c.ConnectionId == connectionId))
+            .FirstOrDefaultAsync();
+    }
+
     public async Task<Message?> GetMessage(int id)
     {
         return await context.Messages.FindAsync(id);
+    }
+
+    public async Task<Group?> GetMessageGroup(string groupName)
+    {
+        return await context.Groups
+                .Include(x => x.Connections)
+                .FirstOrDefaultAsync(x => x.Name == groupName);
     }
 
     public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -50,8 +75,6 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
     public async Task<IEnumerable<MessageDto>> GetMessageThread(string curretUsername, string recipientUsername)
     {
         var messages = await context.Messages
-            .Include(x => x.Sender).ThenInclude(x => x.Photos)
-            .Include(x => x.Recipient).ThenInclude(x => x.Photos)
             .Where(x => 
                 x.RecipientUsername == curretUsername 
                         && x.RecipientDeleted == false 
@@ -60,6 +83,7 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
                         && x.SenderDeleted == false 
                         && x.RecipientUsername == recipientUsername)
             .OrderBy(x => x.MessageSent)
+            .ProjectTo<MessageDto>(mapper.ConfigurationProvider)
             .ToListAsync();
 
         var unreadMessages = messages.Where(x => x.DateRead == null && x.RecipientUsername == curretUsername).ToList();
@@ -70,7 +94,12 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
             await context.SaveChangesAsync();
         }
 
-        return mapper.Map<IEnumerable<MessageDto>>(messages);
+        return messages;
+    }
+
+    public void RemoveConnection(Connection connection)
+    {
+        context.Connections.Remove(connection);
     }
 
     public async Task<bool> SaveAllAsync()
---
+++ Entities/Connection.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API.Entities;
+
+public class Connection
+{
+    public string ConnectionId { get; set; }
+    public required string Username { get; set; }
+
+}
